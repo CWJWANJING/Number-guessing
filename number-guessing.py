833accbc9584ac1b7450f2f81e25071d2f5a70a5
CWJWANJING <wanjingchencwj@gmail.com>
2020-08-06 19:03:49 +0100

    single player completed

--- number-guessing.py
+++ number-guessing.py
@@ -57,20 +57,95 @@ if __name__ == "__main__":
     if int(pnum) == 1:
         print(f"Please guess a number in range 1-100, you have 10 chances:")
         guessnum = random.randint(1,100)
+        chance = 0
         while True:
             inp = input()
             valid = validinp1(inp)
             if valid == False:
                 print("Please enter a valid number")
                 continue
+            elif chance == 10:
+                print("You used up your chances, you lose")
+                break
             else:
                 correct = checknum(int(inp), guessnum)
                 if correct == True:
                     break
                 else:
+                    chance += 1
                     continue
-    # if int(inp) == 2:
-    #     # TODO:
+
+    # local network version
+    # if int(pnum) == 2:
+    #
+    #     # server or not
+    #     print(f"If you want to move first, please enter the word 'server'.")
+    #     pnum = None
+    #     while True:
+    #         pnum = input()
+    #
+    #     guessnum = random.randint(1,100)
+    #
+    #     port = 5546
+    #     move = None
+    #     whoami = None
+    #     conn = None
+    #     try:
+    #         # create a socket
+    #         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #         # if Server
+    #         if pnum == "server":
+    #             ip = "0.0.0.0"
+    #             try:
+    #                 s.bind((ip, port))
+    #             except socket.error as e:
+    #                 print(str(e))
+    #             s.listen(1)
+    #             print("Server started, waiting for connections")
+    #             # set up accept conn
+    #             conn, addr = s.accept()
+    #             conn.setblocking(True)
+    #             print("Connected to:", addr)
+    #             move = "X"
+    #             whoami = "X"
+    #         # else
+    #         else:
+    #             # ask for IP
+    #             ip = sys.argv[1]
+    #             # connect
+    #             s.setblocking(True)
+    #             s.connect((ip,port))
+    #             move = "X"
+    #             whoami = "O"
+    #             conn = s
+    #         # loop
+    #         while True:
+    #             print(f"Please guess a number in range 1-100")
+    #             # if our_turn
+    #             if move == whoami:
+    #                 # get input
+    #                 inp = input()
+    #                 if validinp1(inp) == True:
+    #                     correct = checknum(int(inp), guessnum)
+    #                 else:
+    #                     print("Please enter a valid input")
+    #                     continue
+    #                 # send to server
+    #                 conn.send(str.encode(inp))
+    #             # else
+    #             else:
+    #                 # Receive
+    #                 print("Waiting for the opponent")
+    #                 inp = conn.recv(2048).decode()
+    #
+    #             # check win
+    #             if correct:
+    #                 break
+    #     except Exception as e:
+    #         print(e)
+    #     finally:
+    #         s.close()
+    #
     # if int(inp) == 3:
     #     # TODO:
     # if int(inp) == 4:
